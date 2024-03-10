01cda4611cac60d683b4fec965b2b99d80b931c4
brian098091 <93636904+brian098091@users.noreply.github.com>
2024-03-10 23:42:12 +0800

    Update Loki_aspectual_verb.py

--- Hintat/intent/Loki_aspectual_verb.py
+++ Hintat/intent/Loki_aspectual_verb.py
@@ -57,6 +57,30 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern=""):
         else:
             resultDICT["體貌"].append(1)
 
+    if utterance == "在尿尿":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            resultDICT["體貌"].append(1)
+
+    if utterance == "在看":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            resultDICT["體貌"].append(1)
+
+    if utterance == "好了":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+           resultDICT["體貌"].append(1)
+
+    if utterance == "看一看":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            resultDICT["體貌"].append(1)
+
     if utterance == "有吃":
         if CHATBOT_MODE:
             resultDICT["response"] = getResponse(utterance, args)
