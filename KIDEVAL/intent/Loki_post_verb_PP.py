b8808aa2a16b6785043e24e976d17a6f9750b694
HS6103 <hsuchen1022@gmail.com>
2024-03-13 15:00:42 +0800

    added 動後介詞intent.py, modified main func

--- KIDEVAL/intent/Loki_post_verb_PP.py
+++ KIDEVAL/intent/Loki_post_verb_PP.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 """
-    Loki module for post_verb_PP
+    Loki module for post_verb_pp
 
     Input:
         inputSTR      str,
@@ -32,14 +32,14 @@ except Exception as e:
 responseDICT = {}
 if CHATBOT_MODE:
     try:
-        responseDICT = json.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "reply/reply_post_verb_PP.json"), encoding="utf-8"))
+        responseDICT = json.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "reply/reply_post_verb_pp.json"), encoding="utf-8"))
     except Exception as e:
         print("[ERROR] responseDICT => {}".format(str(e)))
 
 # 將符合句型的參數列表印出。這是 debug 或是開發用的。
 def debugInfo(inputSTR, utterance):
     if DEBUG:
-        print("[post_verb_PP] {} ===> {}".format(inputSTR, utterance))
+        print("[post_verb_pp] {} ===> {}".format(inputSTR, utterance))
 
 def getResponse(utterance, args):
     resultSTR = ""
@@ -51,16 +51,53 @@ def getResponse(utterance, args):
 
 def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern=""):
     debugInfo(inputSTR, utterance)
-    if utterance == "到房間外面":
+    if utterance == "回到家":
         if CHATBOT_MODE:
             resultDICT["response"] = getResponse(utterance, args)
         else:
-            resultDICT["動後PP"].append(1)
+            resultDICT["動後介詞"].append(1)
+            pass
 
-    if utterance == "睡在床上":
+    if utterance == "擺在一起":
         if CHATBOT_MODE:
             resultDICT["response"] = getResponse(utterance, args)
         else:
-            resultDICT["動後PP"].append(1)
+            resultDICT["動後介詞"].append(1)
+            pass
+
+    if utterance == "放在桌子上":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            resultDICT["動後介詞"].append(1)
+            pass
+
+    if utterance == "放在這裡":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            resultDICT["動後介詞"].append(1)
+            pass
+
+    if utterance == "跳到這裡":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            resultDICT["動後介詞"].append(1)
+            pass
+
+    if utterance == "長在泥土裡 ":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            resultDICT["動後介詞"].append(1)
+            pass
+
+    if utterance == "飛到上面":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            resultDICT["動後介詞"].append(1)
+            pass
 
     return resultDICT
