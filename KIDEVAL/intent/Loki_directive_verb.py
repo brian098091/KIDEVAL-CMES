21d736ed358ddbf2519d4fa67b58594e2606525a
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2024-03-17 21:05:21 +0800

    revise directive_verb

--- KIDEVAL/intent/Loki_directive_verb.py
+++ KIDEVAL/intent/Loki_directive_verb.py
@@ -85,6 +85,12 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern=""):
         if CHATBOT_MODE:
             resultDICT["response"] = getResponse(utterance, args)
         else:
+            resultDICT["趨向補語"].append(1)
+            
+    if utterance == "走進門":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
             resultDICT["趨向補語"].append(1)        
         
     return resultDICT
