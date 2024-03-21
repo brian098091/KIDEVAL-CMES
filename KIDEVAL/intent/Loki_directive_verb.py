8c0095a48568dc94865f1e83b7c266e3eef79251
ChenJonathan-tw <chenjonathan901210@gmail.com>
2024-03-21 15:43:21 +0800

    remove pycache and update directive_verb

--- KIDEVAL/intent/Loki_directive_verb.py
+++ KIDEVAL/intent/Loki_directive_verb.py
@@ -91,6 +91,12 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern=""):
         if CHATBOT_MODE:
             resultDICT["response"] = getResponse(utterance, args)
         else:
+            resultDICT["趨向補語"].append(1)
+            
+    if utterance == "飛不起來":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
             resultDICT["趨向補語"].append(1)        
         
     return resultDICT
