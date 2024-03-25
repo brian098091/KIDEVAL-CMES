c26f4808d4a9a3c91211d6d658fa19868fafe22f
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2024-03-25 15:55:51 +0800

    Update Loki_directive_verb.py

--- KIDEVAL/intent/Loki_directive_verb.py
+++ KIDEVAL/intent/Loki_directive_verb.py
@@ -163,6 +163,12 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern=""):
         if CHATBOT_MODE:
             resultDICT["response"] = getResponse(utterance, args)
         else:
+            resultDICT["趨向補語"].append(1)
+            
+    if utterance == "帶他們來":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
             resultDICT["趨向補語"].append(1)        
     
         
