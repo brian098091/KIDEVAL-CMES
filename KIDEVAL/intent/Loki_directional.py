8aa7b10ccd720ec01b116705f7ec3ec6f095418f
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2024-03-25 14:27:07 +0800

    Update Loki_directional.py

--- KIDEVAL/intent/Loki_directional.py
+++ KIDEVAL/intent/Loki_directional.py
@@ -61,6 +61,12 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern=""):
         if CHATBOT_MODE:
             resultDICT["response"] = getResponse(utterance, args)
         else:
+            resultDICT["方位"].append(1)
+            
+    if utterance == "是這裡的嗎":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
             resultDICT["方位"].append(1)        
 
     return resultDICT
