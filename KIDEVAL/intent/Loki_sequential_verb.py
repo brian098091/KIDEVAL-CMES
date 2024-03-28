a37192bb9841d72abdd46bb86999bf1b8123344b
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2024-03-29 01:27:10 +0800

    Update Loki_sequential_verb.py

--- KIDEVAL/intent/Loki_sequential_verb.py
+++ KIDEVAL/intent/Loki_sequential_verb.py
@@ -106,6 +106,12 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern=""):
         if CHATBOT_MODE:
             resultDICT["response"] = getResponse(utterance, args)
         else:
+            resultDICT["連謂/兼語"].append(1)
+            
+    if utterance == "幫我的連起來":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
             resultDICT["連謂/兼語"].append(1)        
 
     return resultDICT
