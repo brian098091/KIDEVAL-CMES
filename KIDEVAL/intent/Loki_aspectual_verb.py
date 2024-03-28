43cfe2b863987441cf86cc9b57e51302ce330cd7
brian098091 <93636904+brian098091@users.noreply.github.com>
2024-03-29 01:17:01 +0800

    Update Loki_aspectual_verb.py

--- KIDEVAL/intent/Loki_aspectual_verb.py
+++ KIDEVAL/intent/Loki_aspectual_verb.py
@@ -87,11 +87,6 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern=""):
         else:
             resultDICT["體貌"].append(1)
 
-    if utterance == "可以了":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            resultDICT["體貌"].append(1)
 
     if utterance == "喝著":
         if CHATBOT_MODE:
